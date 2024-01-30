21c74d47643c32c1aa29e9ad27898c4b9cc704b4
Diego Sena <diego.souza.sena10@gmail.com>
2024-01-30 13:32:39 -0300

    *

--- utils/str.py
+++ utils/str.py
@@ -5,9 +5,7 @@ def unitByteConversion(bytes: int):
   """unitByteConversion(100) -> \"100 B\""""
   UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
   i = 0
-  while (1):
-    if bytes <= pow(1024, i + 1):
-      break
+  while (bytes > pow(1024, i + 1)):
     i += 1
   result = round(bytes / pow(1024, i), 2)
   return f'{result} {UNITS[i]}'
